cc6ed013a69b365f12362ed5d39f70c4ea1cc4e1
Carlos Ramirez <calbertora@gmail.com>
2020-03-16 21:14:59 -0500

    Add the reader

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace ConsoleAppFromMicrosoft
+namespace TeleprompterConsole
 {
     class Program
     {
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using(var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }    
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
