81f1bb5a683f46cfeb418e8b77ba9a17b8e1f988
Carlos Ramirez <calbertora@gmail.com>
2020-03-16 20:57:57 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppFromMicrosoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
